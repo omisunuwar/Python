a84df221c9cbc6e017dd91f857168704d9eebea9
Omi Sunuwar <43663305+omisunuwar@users.noreply.github.com>
2021-09-15 15:44:48 -0500

    Update Ex10-2.py

--- Ex10-2.py
+++ Ex10-2.py
@@ -5,34 +5,26 @@ hrs=list()
 
 for line in fhand:
     line.rstrip()
-    #if not line.startswith('From'):continue
-    #print(line)
     words=line.split()
-    if line=='':continue #to ignore lines that start with blank space
-    if len(words)<3:continue  # to ignore From: with 2 words
+    if line==' ':continue #to ignore lines that start with blank space
+    if len(words)<3:continue  # to ignore lines with 2 words
     if not line.startswith('From'):continue
-    #if words[0]!='From':continue #Traceback:index is non-existant
-    #word in words and w in word
-    #print(line)
     atpos=line.find(':')
     newword=line[atpos-2:atpos]
     hrs.append(newword)
-hrs.sort() #sort key i.e. in alphabetically(in increasing order)
-#print(hrs)
+    
+hrs.sort() #sort key i.e. in alphabetically increasing order
 
-#for making histogram of two tupple from list of hrs
+#for making histogram 
 counts=dict()
 for hr in hrs:
     counts[hr]=counts.get(hr,0)+1
-print(counts.items()) #counts.items()gives key, value pair i.e. tupple
     
-#to print tupple    
+ #counts.items()gives two tupple of key, value 
+ #to print tupple    
 for key,val in counts.items():
   print(key,val)
 
-
-
-#########################################################
 #to create reverse tupple to sort accouding to counts  
 lst=list()
 for key,val in counts.items():
